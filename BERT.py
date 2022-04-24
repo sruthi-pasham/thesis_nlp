4c170b30934972e90716122b0e28d78e10305009
Pedro69491 <pnrmc@outlook.pt>
2022-04-25 00:01:19 +0200

    Bert file without metrics to infer results

--- BERT.py
+++ BERT.py
@@ -1,10 +1,19 @@
 import numpy as np
+import transformers
 import torch
-from transformers import BertTokenizer, BertModel
+from transformers import AdamW, BertTokenizer, BertForSequenceClassification
 from merger import emoji_pattern, train_df, valid_df, test_df, posts, y_test, y_valid, y_train
+from courses import courses
 import re
+from torch import nn
+from torch.utils.data import DataLoader
 
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+
+tokenizer = BertTokenizer.from_pretrained('./')
+#tokenizer.save_pretrained('./')
+torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 
 
 def bert_cleaner(partition):
@@ -18,6 +27,7 @@ def bert_cleaner(partition):
     return lst
 
 
+
 def tokenize(partition):
     old_lst = bert_cleaner(partition)
     new_lst = []
@@ -26,17 +36,26 @@ def tokenize(partition):
         if dim < 512:
             new_lst.append(tokenizer(post, padding='max_length', truncation=True, max_length=512))
         else:
-            start = round((dim-512)/2)
-            new_lst.append(tokenizer(post[start:start+512]))
+            new_lst.append(split_input(post,dim))
     return new_lst
-            
 
 
+def split_input(post, dim):
+    start = round((dim-512)/2)
+    token = tokenizer(post)
+    for i in token.keys():
+        token[i] = token[i][start:start+512]
+    return token
+           
+
+#turn this into a function
 tokens = tokenize(train_df)
-train = {}
+valid = {}
 keys = ['input_ids', 'token_type_ids', 'attention_mask']
 for key in keys:
-    train[key] = np.array([torch.tensor(item[key]) for item in tokens])
+    valid[key] = np.array([torch.tensor(item[key]) for item in tokens])
+
+
 
 
 class jobs_template_dataset(torch.utils.data.Dataset):
@@ -53,13 +72,39 @@ class jobs_template_dataset(torch.utils.data.Dataset):
   def __len__(self):
     return len(self.labels)
 
-train_vals = jobs_template_dataset(train,y_train)
+
+valid_dataset = jobs_template_dataset(valid,y_valid)
+
+#model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=len(courses), from_tf=True)
+model = BertForSequenceClassification.from_pretrained('./')
+#model.save_pretrained('./')
+
+#model = model.to(device='device')
+
+def binary_classification(inputs, targets):
+    criterion = nn.BCEWithCrossEntropyLoss()
+    return criterion(inputs, targets)
 
 
+valid_loader = DataLoader(valid_dataset, batch_size=32)
+
+optim = AdamW(model.parameters(), lr=0.0001)
 
 
+model.train()
+for batch in valid_loader:
+    optim.zero_grad()
     
+    inputs = batch['input_ids']
+    attention = batch['attention_mask']
+    labels = batch['labels']
 
+    logits = model(inputs, attention)
+
+    loss = binary_classification(logits, labels)
+
+    loss.backward()
+    optim.step()
 
 
 
